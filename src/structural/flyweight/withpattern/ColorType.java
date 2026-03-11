f5cd18c54a814b3ad257d30efc8ce939722a6e68
Leovelbert V <leovelbertv@gmail.com>
2026-03-11 22:46:21 +0530

    Design Patterns code Changes.

--- src/structural/flyweight/withpattern/ColorType.java
+++ src/structural/flyweight/withpattern/ColorType.java
@@ -1,6 +1,7 @@
 package structural.flyweight.withpattern;
-
+//FlyWeight
 public class ColorType {
+    //Intrinsic Property (means it will be shared all the modules)
     private String color;
 
     public ColorType(String color) {
